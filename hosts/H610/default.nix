fefeda7bd61aa31ace39ca5f57220460e2c6313e
Hank Hogan <ysh2291939848@outlook.com>
2025-02-20 23:15:01 +0800

    cock

--- hosts/H610/default.nix
+++ hosts/H610/default.nix
@@ -52,6 +52,12 @@
 
   services.spice-vdagentd.enable = true;
 
+  services.cockpit = {
+    enable = true;
+    openFirewall = true;
+    port = 9091;
+  };
+
   services.ollama = {
     enable = true;
     acceleration = "rocm";
