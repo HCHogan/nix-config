987f9e2639043d0f4db71ad953af513193514432
Hank Hogan <ysh2291939848@outlook.com>
2025-03-01 16:53:14 +0800

    kvm osx

--- hosts/b660/default.nix
+++ hosts/b660/default.nix
@@ -20,6 +20,12 @@
     preferStaticEmulators = true;
   };
 
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
+
   networking.hostName = "b660"; # Define your hostname.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
