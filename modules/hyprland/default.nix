75ec97fed4ec42a922875f3d1026de2c8a005a4c
Hank Hogan <ysh2291939848@outlook.com>
2025-03-01 15:26:08 +0800

    use kitty

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -207,7 +207,7 @@ in {
           "$mod, F, fullscreen"
           "$mod, Space, togglefloating"
           "$mod SHIFT, S, exec, grimblast copy area"
-          "$mod, Return, exec, wezterm"
+          "$mod, Return, exec, kitty"
           "$mod, I, exec, gnome-control-center"
           "$mod, E, exec, nautilus"
           "$mod, W, exec, zen"
