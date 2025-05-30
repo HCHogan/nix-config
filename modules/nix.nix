025a032c4ed24d156284e20634eb98ca3bbebd34
hank <ysh2291939848@outlook.com>
2025-05-30 15:17:34 +0800

    update

--- modules/nix.nix
+++ modules/nix.nix
@@ -30,7 +30,6 @@
     substituters = [
       "https://cache.garnix.io"
       "https://hyprland.cachix.org"
-      "https://attic.xuyh0120.win/lantian"
       "https://cache.nixos.org"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cosmic.cachix.org/"
@@ -38,7 +37,6 @@
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
     experimental-features = ["nix-command" "flakes"];
