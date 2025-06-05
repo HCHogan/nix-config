c70e6f8e6527e62cdbadba00fc11bc2218c10e91
Hank Hogan <ysh2291939848@outlook.com>
2025-06-05 12:22:26 +0800

    update

--- modules/nix.nix
+++ modules/nix.nix
@@ -29,12 +29,14 @@
   nix.settings = {
     substituters = [
       "https://cache.garnix.io"
+      "https://cache.iog.io"
       "https://hyprland.cachix.org"
       "https://cache.nixos.org"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cosmic.cachix.org/"
     ];
     trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
