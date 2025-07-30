acd1dd26039abe6dd7eed3c9d1475a1771568b9d
Hank Hogan <ysh2291939848@outlook.com>
2025-07-30 19:19:34 +0800

    update

--- home/hank.nix
+++ home/hank.nix
@@ -32,7 +32,7 @@ in {
         rebase-all = ["rebase" "-s" "all:roots(trunk()..mutable())" "-d" "trunk()"];
       };
       ui = {
-        diff.format = "git";
+        diff-formatter = "git";
         paginate = "never";
         default-command = "log";
       };
