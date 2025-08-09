30e7c2010cfab60135aba1bcfc14bf5cde882f14
hank <ysh2291939848@outlook.com>
2025-08-09 13:43:08 +0800

    update

--- hosts/m3max/default.nix
+++ hosts/m3max/default.nix
@@ -48,4 +48,13 @@
     ];
   };
 
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    package = pkgs.postgresql_17;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+  };
 }
