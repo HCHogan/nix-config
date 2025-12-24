bbaf4e5fc57fefb8ad01fcd9d532684f3f431c3a
Hank Hogan <ysh2291939848@outlook.com>
2025-12-24 14:54:45 +0800

    update

--- nixos/modules/minecraft/gate.nix
+++ nixos/modules/minecraft/gate.nix
@@ -2,58 +2,33 @@
   config,
   pkgs,
   ...
-}: {
-  environment.systemPackages = [pkgs.gate];
-
-  environment.etc."gate/config.yml".text = ''
+}: let
+  gateConfig = pkgs.writeText "gate.yml" ''
     config:
-      bind: 0.0.0.0:25555
+      bind: 0.0.0.0:25566
+
     lite:
       enabled: true
       routes:
         - host: "*"
-          backend: 10.0.0.66:25565
-          proxyProtocol: true
-          fallback:
-            motd: |
-              §cBackend offline
-              §eTry again later
-            version:
-              name: '§cOffline'
-              protocol: -1
+          backend: "10.0.0.66:25565"
   '';
+in {
+  virtualisation.podman.enable = true;
+  virtualisation.oci-containers.backend = "podman";
 
-  users.groups.gate = {};
-  users.users.gate = {
-    isSystemUser = true;
-    group = "gate";
+  virtualisation.oci-containers.containers.gate = {
+    image = "ghcr.io/minekube/gate:latest";
+    autoStart = true;
+
+    extraOptions = ["--network=host"];
+
+    volumes = [
+      "${gateConfig}:/config.yml:ro"
+    ];
+
+    cmd = ["-c" "/config.yml"];
   };
 
-  systemd.services.gate = {
-    description = "Minekube Gate (Lite Mode)";
-    wants = ["network-online.target" "wg-quick-wg0.service"];
-    after = ["network-online.target" "wg-quick-wg0.service"];
-
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig = {
-      User = "gate";
-      Group = "gate";
-
-      StateDirectory = "gate";
-      WorkingDirectory = "/var/lib/gate";
-
-      ExecStart = "${pkgs.gate}/bin/gate -c /etc/gate/config.yml";
-      Restart = "always";
-      RestartSec = "2s";
-
-      # 基本加固（可删）
-      NoNewPrivileges = true;
-      PrivateTmp = true;
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-    };
-  };
+  networking.firewall.allowedTCPPorts = [25566];
 }
