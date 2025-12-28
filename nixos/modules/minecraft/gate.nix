d3cd50e7efbbf7b19e19c779be30c794a0a10312
Hank Hogan <ysh2291939848@outlook.com>
2025-12-28 17:36:34 +0800

    update

--- nixos/modules/minecraft/gate.nix
+++ nixos/modules/minecraft/gate.nix
@@ -6,13 +6,29 @@
   gateConfig = pkgs.writeText "gate.yml" ''
     config:
       bind: 0.0.0.0:25566
+      onlineMode: false
 
-    lite:
-      enabled: true
-      routes:
-        - host: "*"
-          backend: "10.0.0.66:25565"
+      servers:
+        ftb: 10.0.0.66:25571
+
+      try:
+        - ftb
+
+      forwarding:
+        mode: legacy
+
+      status:
+        motd: |
+          §bGate -> CatServer
+          §e10.0.0.66:25571
+        showMaxPlayers: 10000
+        logPingRequests: false
   '';
+  # lite:
+  #   enabled: true
+  #   routes:
+  #     - host: "*"
+  #       backend: 10.0.0.66:25565
 in {
   virtualisation.podman.enable = true;
   virtualisation.oci-containers.backend = "podman";
