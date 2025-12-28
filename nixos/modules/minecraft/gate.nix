c41e70569f396934559f4e0a7c248a723a3a0138
Hank Hogan <ysh2291939848@outlook.com>
2025-12-28 18:21:03 +0800

    update

--- nixos/modules/minecraft/gate.nix
+++ nixos/modules/minecraft/gate.nix
@@ -5,30 +5,16 @@
 }: let
   gateConfig = pkgs.writeText "gate.yml" ''
     config:
-      bind: 0.0.0.0:25566
+      bind: 0.0.0.0:25565
       onlineMode: false
 
-      servers:
-        ftb: 10.0.0.66:25571
-
-      try:
-        - ftb
-
-      forwarding:
-        mode: legacy
-
-      status:
-        motd: |
-          §bGate -> CatServer
-          §e10.0.0.66:25571
-        showMaxPlayers: 10000
-        logPingRequests: false
+      lite:
+        enabled: true
+        routes:
+          - host: "sh.imdomestic.com"
+            backend: 10.0.0.66:25565
+            proxyProtocol: true
   '';
-  # lite:
-  #   enabled: true
-  #   routes:
-  #     - host: "*"
-  #       backend: 10.0.0.66:25565
 in {
   virtualisation.podman.enable = true;
   virtualisation.oci-containers.backend = "podman";
