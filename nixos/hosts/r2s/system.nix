915847e22d2ecf08da31e3956514ad95b4d61902
Hank Hogan <ysh2291939848@outlook.com>
2026-01-07 20:55:00 +0800

    update

--- nixos/hosts/r2s/system.nix
+++ nixos/hosts/r2s/system.nix
@@ -47,7 +47,11 @@
       };
     };
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [ "nf_flow_table_pppoe" ];
+    kernelModules = [
+      "nf_flow_table_pppoe"
+      "tcp_bbr"
+      "nf_conntrack"
+    ];
     kernelParams = [
       "console=ttyS2,1500000"
       "earlycon=uart8250,mmio32,0xff130000"
@@ -62,28 +66,52 @@
     tmp.useTmpfs = true;
     growPartition = true;
     kernel.sysctl = {
-      "vm.vfs_cache_pressure" = 10;
-      "vm.dirty_ratio" = 50;
-      "vm.swappiness" = 20;
+      "net.core.default_qdisc" = "cake";
+      "net.core.somaxconn" = 65536;
+      "net.core.netdev_max_backlog" = 10000;
+      "net.core.netdev_budget" = 600;
+      "net.core.rps_sock_flow_entries" = 32768;
+      "net.core.dev_weight" = 600;
 
-      "net.ipv4.ip_forward" = 1;
-      "net.ipv6.conf.all.forwarding" = 1;
-      "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.tcp_fastopen" = 3;
+      "net.ipv4.tcp_keepalive_time" = 60;
+      "net.ipv4.tcp_keepalive_intvl" = 10;
+      "net.ipv4.tcp_keepalive_probes" = 6;
+      "net.ipv4.tcp_mtu_probing" = true;
 
-      "net.core.netdev_max_backlog" = 16384;
+      # tcp pending
+      "net.ipv4.tcp_max_syn_backlog" = 65536;
+      "net.ipv4.tcp_max_tw_buckets" = 2000000;
+      "net.ipv4.tcp_tw_reuse" = true;
+      "net.ipv4.tcp_fin_timeout" = 10;
+      "net.ipv4.tcp_slow_start_after_idle" = false;
 
-      "net.core.rmem_max" = 4194304;
-      "net.core.wmem_max" = 4194304;
-      "net.ipv4.tcp_rmem" = "4096 87380 4194304";
-      "net.ipv4.tcp_wmem" = "4096 16384 4194304";
+      # net mem
+      "net.core.rmem_default" = 1048576;
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_default" = 1048576;
+      "net.core.wmem_max" = 16777216;
+      "net.core.optmem_max" = 65536;
+      "net.ipv4.tcp_rmem" = "4096 1048576 2097152";
+      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+      "net.ipv4.udp_rmem_min" = 8192;
+      "net.ipv4.udp_wmem_min" = 8192;
 
-      "net.netfilter.nf_conntrack_max" = 65536;
-      "net.netfilter.nf_conntrack_tcp_timeout_established" = 7440;
+      "net.ipv4.ip_forward" = true;
+      "net.ipv4.conf.all.forwarding" = true;
+      "net.ipv4.conf.default.forwarding" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+      "net.ipv6.conf.default.forwarding" = true;
+      "net.ipv4.conf.all.rp_filter" = true;
+      "net.ipv4.conf.default.rp_filter" = true;
 
-      "net.ipv4.neigh.default.gc_thresh1" = 1024;
-      "net.ipv4.neigh.default.gc_thresh2" = 2048;
-      "net.ipv4.neigh.default.gc_thresh3" = 4096;
+      "net.netfilter.nf_conntrack_buckets" = 393216;
+      "net.netfilter.nf_conntrack_max" = 393216;
+      "net.netfilter.nf_conntrack_generic_timeout" = 60;
+      "net.netfilter.nf_conntrack_tcp_timeout_fin_wait" = 10;
+      "net.netfilter.nf_conntrack_tcp_timeout_established" = 600;
+      "net.netfilter.nf_conntrack_tcp_timeout_time_wait" = 5;
     };
   };
 
@@ -219,6 +247,36 @@
         EmitDNS = true; # send DNS with RA
       };
     };
+  };
+
+  systemd.services."tweak-network-settings" = {
+    description = "Tweak network settings";
+    serviceConfig = {
+      Type = "simple";
+    };
+    wantedBy = ["multi-user.target"];
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    script = ''
+      # The {Rx,Tx}BufferSize systemd.link options for intern0 is not working, set manually
+      ${pkgs.ethtool}/bin/ethtool -G end0 rx 1024
+      ${pkgs.ethtool}/bin/ethtool -G end0 tx 1024
+
+      ## Figure out why use these CPU mask combination?
+
+      # 8(0b1000, CPU3) for 24(xhci-hcd:usb4, extern0)
+      echo 8 > /proc/irq/24/smp_affinity
+      # 2(0b0010, CPU1) for 47(intern0)
+      echo 2 > /proc/irq/47/smp_affinity
+
+      # 7(0b0111, CPU0-2)
+      echo 7 > /sys/class/net/end0/queues/rx-0/rps_cpus
+      # d(0b1101, CPU0,CPU2-3)
+      echo d > /sys/class/net/enu1/queues/rx-0/rps_cpus
+
+      echo 2048 > /sys/class/net/extern0/queues/rx-0/rps_flow_cnt
+      echo 2048 > /sys/class/net/intern0/queues/rx-0/rps_flow_cnt
+    '';
   };
 
   systemd.services.network-rps = {
