d82a0eaf5ce325aa5e6a2ac03b8ced9bf1c58188
Hank Hogan <ysh2291939848@outlook.com>
2026-01-11 15:06:54 +0800

    update

--- nixos/hosts/r5sjp/system.nix
+++ nixos/hosts/r5sjp/system.nix
@@ -168,7 +168,17 @@ in {
     description = "hank";
     extraGroups = [
       "wheel"
+      "podman"
     ];
+  };
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
   };
 
   i18n.defaultLocale = "en_GB.UTF-8";
