f4fb1d2bfae486da311388335edc9951eba17459
Hank Hogan <ysh2291939848@outlook.com>
2026-01-29 15:11:18 +0800

    update

--- home/users/linwhite/default.nix
+++ home/users/linwhite/default.nix
@@ -1,1 +1,14 @@
-{...}: {}
+{
+  lib,
+  inputs,
+  system,
+  pkgs,
+  ...
+}: {
+  home.sessionVariables = {
+    ZDOTDIR =
+      if lib.hasInfix "darwin" system
+      then "/Users/linwhite/.config/zsh"
+      else "/home/linwhite/.config/zsh";
+  };
+}
