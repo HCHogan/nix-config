3db7e889793c7176a64a9199e23812e47c6e7b63
Hank Hogan <ysh2291939848@outlook.com>
2026-02-05 10:50:04 +0800

    update

--- flake.nix
+++ flake.nix
@@ -51,6 +51,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    NixVirt = {
+      url = "https://flakehub.com/f/AshleyYakeley/NixVirt/*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
     };
