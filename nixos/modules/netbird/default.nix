2e82cf9d8edcb1fe2e63f5a8635492f675b318c8
Hank Hogan <ysh2291939848@outlook.com>
2026-02-16 18:21:36 +0800

    update

--- nixos/modules/netbird/default.nix
+++ nixos/modules/netbird/default.nix
@@ -102,7 +102,7 @@ in {
       proxyPass = "http://127.0.0.1:39995";
       proxyWebsockets = true;
       extraConfig = ''
-        proxy_set_header Host $http_host;
+        proxy_set_header Host $host:$server_port;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header X-Real-IP $remote_addr;
