1aac07f754b6aa867b64c30a2b168c488fb7c3f6
Hank Hogan <ysh2291939848@outlook.com>
2026-02-17 12:28:13 +0800

    update

--- nixos/modules/netbird/default.nix
+++ nixos/modules/netbird/default.nix
@@ -149,7 +149,7 @@ in {
         settings = {
           AUTH_AUTHORITY = "https://${authDomain}:${toString httpsPort}";
           AUTH_CLIENT_ID = netbirdClientId;
-          # AUTH_CLIENT_SECRET = lib.mkForce "23ddPSMhzr3lkxtgljDnFngCApUYNkSKicM2RieeeWIblXHzcO4UFdXUXqrb8aZZ";
+          AUTH_CLIENT_SECRET = lib.mkForce "23ddPSMhzr3lkxtgljDnFngCApUYNkSKicM2RieeeWIblXHzcO4UFdXUXqrb8aZZ";
           AUTH_AUDIENCE = netbirdClientId;
 
           # 强烈建议显式给出，否则 envsubst 变量可能为空
