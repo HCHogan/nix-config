ea792ed7d600f042e7e2532400cbe15708a5adb8
Hank Hogan <ysh2291939848@outlook.com>
2026-02-25 13:52:01 +0800

    update

--- nixos/modules/ssh/default.nix
+++ nixos/modules/ssh/default.nix
@@ -1,6 +1,8 @@
 {lib, ...}: let
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINR7+B5jirLm5+cXrKabb0hrvq1OFxX6jCzKi/Sb4rkj ysh2291939848@outlook.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/MFKoIlH1i0YDAnIoHQKmKEKFGcKa1V4gET/bYifcd ysh2291939848@outlook.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG290WBMVGWxpye7MliOTbiCZAd3mbi/Q9sFkBLE2Vno ysh2291939848@outlook.com"
   ];
 in {
   environment.etc."ssh/authorized_keys.d/master" = {
@@ -17,13 +19,13 @@ in {
     };
   };
 
-  programs.ssh = {
-    extraConfig = ''
-      CanonicalizeHostname yes
-      CanonicalizeMaxDots 0
-      CanonicalDomains imdomestic.com
-      CanonicalizeFallbackLocal no
-      CanonicalizeFallbackLocal yes
-    '';
-  };
+  # programs.ssh = {
+  #   extraConfig = ''
+  #     CanonicalizeHostname yes
+  #     CanonicalizeMaxDots 0
+  #     CanonicalDomains imdomestic.com
+  #     CanonicalizeFallbackLocal no
+  #     CanonicalizeFallbackLocal yes
+  #   '';
+  # };
 }
